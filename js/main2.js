45b58725e1f5a14e979d53610c36e0f6d789fc9c
Mert Genç <162042618+Mertgencc@users.noreply.github.com>
2026-05-03 01:58:05 +0300

    LocalStorage Eklendi

--- js/main2.js
+++ js/main2.js
@@ -1,3 +1,15 @@
+let notes = [];
+
+window.onload() = function(){
+    const data = localStorage.getItem("notes");
+
+    if (data){
+        notes = JSON.parse(data);
+    }
+
+    renderNotes();
+}
+
 function addNote() {
     const input = document.getElementById("textInput");
     const value = input.value;
@@ -7,15 +19,46 @@ function addNote() {
     const li = document.createElement("li");
     li.textContent = value;
 
+    notes.push(value);
+    localStorage.getItem("notes", JSON.stringify(notes));
+
     const btn = document.createElement("button");
     btn.textContent = "NOTU SİL";
 
     btn.onclick = function(){
-        li.remove();
+        notes = notes.filter((n) => n !== value);
+        localStorage.getItem("notes", JSON.stringify(notes));
+        renderNotes();
     }
 
     li.appendChild(btn);
 
     document.getElementById("not").appendChild(li);
     input.value = "";
+}
+
+function renderNotes(){
+    const list = document.getElementById("notes");
+
+    list.innerHTML();
+
+    notes.forEach(function(notes){
+        const li = document.createElement("li");
+
+        li.textContent = "notes";
+
+        const btn = document.createElement("button");
+
+        btn.textContent = "SİL";
+
+        btn.onclick() = function(){
+            notes = notes.filter((n) => n !== value);
+            localStorage.getItem("notes", JSON.stringify(notes));
+            renderNotes();
+        }
+
+        li.appendChild(btn);
+        list.appendChild(li);
+
+    })
 }
